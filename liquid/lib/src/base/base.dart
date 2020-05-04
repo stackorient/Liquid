523c8f128b618f7187f2f7b6822778ef50203d95
Raj <rs457036@gmail.com>
2020-05-04 19:05:52 +0530

    refactor codes

--- liquid/lib/src/base/base.dart
+++ liquid/lib/src/base/base.dart
@@ -3,7 +3,6 @@ export 'src/colors.dart';
 export 'src/liquid_layout.dart';
 export 'src/liquid_splash_factory.dart';
 export 'src/liquid_state_manager.dart';
-export 'src/liquid_text.dart';
 export 'src/liquid_theme.dart';
 export 'src/typography.dart';
 export 'src/liquid_box.dart';
