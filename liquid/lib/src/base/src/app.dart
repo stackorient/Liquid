efc07b0b9883ef5081adf928f2ef82d11afc0e6e
Raj <rs457036@gmail.com>
2020-05-26 20:22:20 +0530

    updated name

--- liquid/lib/src/base/src/app.dart
+++ liquid/lib/src/base/src/app.dart
@@ -3,7 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:flutter/material.dart';
-import 'package:liquid/src/base/src/ltext/ltext.dart';
+import 'ltext/ltext.dart';
 
 import 'liquid_splash_factory.dart';
 import 'liquid_theme.dart';
